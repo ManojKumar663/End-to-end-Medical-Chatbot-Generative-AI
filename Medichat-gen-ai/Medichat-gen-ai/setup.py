4b0e81a0168a1ff6bc320e9d5a1017ff66e0c92e
MANOJ KUMAR <manoj006mk663@gmail.com>
2025-06-01 23:03:38 +0530

    Update setup.py

--- Medichat-gen-ai/Medichat-gen-ai/setup.py
+++ Medichat-gen-ai/Medichat-gen-ai/setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name = 'Generative AI Project',
     version = '0.0.0',
-    author = 'Ravikumar',
-    author_email= 'ravikumaral0311@gmail.com',
+    author = 'Manoj Kumar',
+    author_email= 'manoj006mk663@gmail.com',
     packages= find_packages(),
     install_requires = []
 )
